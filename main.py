3139c7c5765aa453c58a8220c058d1b3fd55de10
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-06 20:09:42 +0300

    main.py new functions load and save

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import json
+
+client_info = {}
+
+def load():
+    global client_info
+    with open('client_info.json', 'r', encoding='utf-8') as json_file:
+        client_info = json.load(json_file)
+
+def save():
+    with open('client_info.json', 'w', encoding='utf-8') as outfile:
+        json.dump(client_info, outfile)
+
+
 if __name__ == '__main__':
     command = ""
     while command != '10':
@@ -10,3 +24,5 @@ if __name__ == '__main__':
             print('До свидания.')
         else:
             print('Действие не распознано. Попробуйте ещё раз.')
+
+
