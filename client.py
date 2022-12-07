8f6ed8594966c018c4918a952f92ee163dbb6ad5
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-07 21:52:21 +0300

    Error

--- client.py
+++ client.py
@@ -8,22 +8,18 @@ def load():
         client_info = json.load(json_file)
 
 def show_info():
+    i = 0
     print("Информация о счетах")
     print("----------------------------------")
-    print("Имя:", client_info['accounts'][0]['name'])
-    print("Платёжная система:", client_info['accounts'][0]['system'])
-    print("Номер:", client_info['accounts'][0]['number'])
-    print("Тип:", client_info['accounts'][0]['type'])
-    print("Баланс:", client_info['accounts'][0]['balance'])
-    print("Срок действия до", client_info['accounts'][0]['validity period'])
-    print("----------------------------------")
-    print("Имя:", client_info['accounts'][1]['name'])
-    print("Платёжная система:", client_info['accounts'][1]['system'])
-    print("Номер:", client_info['accounts'][1]['number'])
-    print("Тип:", client_info['accounts'][1]['type'])
-    print("Баланс:", client_info['accounts'][1]['balance'])
-    print("Срок действия до", client_info['accounts'][1]['validity period'])
-    print("----------------------------------")
+    for accounts in client_info:
+        print("Имя:", client_info['accounts'][i]['name'])
+        print("Платёжная система:", client_info['accounts'][i]['system'])
+        print("Номер:", client_info['accounts'][i]['number'])
+        print("Тип:", client_info['accounts'][i]['type'])
+        print("Баланс:", client_info['accounts'][i]['balance'])
+        print("Срок действия до", client_info['accounts'][i]['validity period'])
+        print("----------------------------------")
+        i += 1
 
 load()
 show_info()
