dc8326f0a5ae251b7a437e07fddcfd0e9588efa3
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-07 22:10:55 +0300

    Changes client.py show_info

--- client.py
+++ client.py
@@ -8,17 +8,15 @@ def load():
         client_info = json.load(json_file)
 
 def show_info():
-    i = 0
-    l = len(client_info['accounts'])
     print("Информация о счетах")
     print("----------------------------------")
     for accounts in client_info['accounts']:
-        print("Имя:", client_info['accounts'][i]['name'])
-        print("Платёжная система:", client_info['accounts'][i]['system'])
-        print("Номер:", client_info['accounts'][i]['number'])
-        print("Тип:", client_info['accounts'][i]['type'])
-        print("Баланс:", client_info['accounts'][i]['balance'])
-        print("Срок действия до", client_info['accounts'][i]['validity period'])
+        print("Имя:", accounts['name'])
+        print("Платёжная система:", accounts['system'])
+        print("Номер:", accounts['number'])
+        print("Тип:", accounts['type'])
+        print("Баланс:", accounts['balance'])
+        print("Срок действия до", accounts['validity period'])
         print("----------------------------------")
 
 load()
