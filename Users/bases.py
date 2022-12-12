1a6e365d0ebc317deaa3ad42cf5c6986e621b5c0
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-12 16:02:57 +0300

    Full base.

--- Users/bases.py
+++ Users/bases.py
@@ -5,15 +5,17 @@ fake = Faker(locale="ru_RU")
 
 def load():
     global users_info
-    with open('users_info.json', 'r', encoding='utf-8') as json_file:
+    with open('users_info_out.json', 'r', encoding='utf-8') as json_file:
         users_info = json.load(json_file)
 
 def add():
     for i in range(100):
         users_info['Users'].append(fake.profile())
-# print(fake.profile())
+        del users_info['Users'][i]['current_location']
+        del users_info['Users'][i]['birthdate']
+
 def save():
-    with open('users_info.json', 'w', encoding='utf-8') as outfile:
+    with open('users_info_out.json', 'w', encoding='utf-8') as outfile:
         json.dump(users_info, outfile)
 
 load()
