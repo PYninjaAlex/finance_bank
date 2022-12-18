230e683509a2e8eb2f60d6b4a87b8ecbf3a02e14
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-18 19:33:16 +0300

    First SELECT.

--- Users/db.py
+++ Users/db.py
@@ -1,8 +1,12 @@
+import random
 import sqlite3 as sq
+from faker import Faker
 
-with sq.connect('users.db') as con:
-    cur = con.cursor()
+def drop_table():
     cur.execute('DROP TABLE IF EXISTS users')
+
+
+def create_table():
     cur.execute('''CREATE TABLE IF NOT EXISTS users(
         name TEXT NOT NULL,
         sex TEXT,
@@ -13,11 +17,46 @@ with sq.connect('users.db') as con:
         username TEXT,
         adress TEXT
         )''')
-    cur.execute('''INSERT INTO users (name, sex, age, mail, job, company, username, adress)
-     VALUES ("user_1", "F", 12, "skyportsoul@mail.ru", "programmer", "Yandex", "PYninjaAlex", "Krasnormeyskay str." )''')
 
+def new_table():
     cur.execute('DROP TABLE IF EXISTS id')
     cur.execute('''CREATE TABLE IF NOT EXISTS id(
         user_1_id TEXT NOT NULL
         )''')
     cur.execute('INSERT INTO id (user_1_id) VALUES (532131) ')
+
+def add():
+    for i in range(100):
+        user = fake.profile()
+        cur.execute(f''' 
+                INSERT INTO users VALUES(
+                "{user["name"]}",
+                "{user["sex"]}",
+                {random.randint(10, 35)},
+                "{user["mail"]}",
+                "{user["job"]}",
+                "{user["company"]}",
+                "{user["username"]}",
+                "{user["address"]}"
+                )    
+            ''')
+
+fake = Faker(locale="ru_RU")
+
+
+with sq.connect('users.db') as con:
+    cur = con.cursor()
+    # drop_table()
+    # create_table()
+    # add()
+    # new_table()
+
+cur.execute('''
+    SELECT name, sex, age FROM users 
+    WHERE sex = "M" 
+    ORDER BY age DESC 
+    LIMIT 5 OFFSET 10
+''')
+
+for result in cur:
+    print(result)
