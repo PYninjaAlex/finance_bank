86d49146ee655e5d2840f0c712bffa1e673e3f2e
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-12-20 20:42:58 +0300

    Everything in function.

--- Users/db.py
+++ Users/db.py
@@ -2,6 +2,7 @@ import random
 import sqlite3 as sq
 from faker import Faker
 
+cur = None
 def drop_table():
     cur.execute('DROP TABLE IF EXISTS users')
 
@@ -43,31 +44,64 @@ def add():
 
 fake = Faker(locale="ru_RU")
 
-
-with sq.connect('users.db') as con:
-    cur = con.cursor()
-    # drop_table()
-    # create_table()
-    # add()
-    # new_table()
-print('First select =>\n')
-cur.execute('''
-    SELECT name, sex, age FROM users
-    WHERE sex = "M" and
-    age BETWEEN 10 AND 15
+def update_student():
+    cur.execute('''
+    UPDATE users 
+    SET job = 'student' 
+    WHERE age 
+    BETWEEN 16 AND 21
 ''')
 
-for result in cur:
-    print(result)
 
-print('\nSecond select =>\n')
+def delete_user_name():
+    cur.execute('''
+    DELETE FROM users
+    WHERE name = 'Пелагея Ивановна Суворова'
+''')
 
-cur.execute('''
-    SELECT name FROM users
+
+def set_req():
+    cur.execute('''
+    UPDATE users 
+    SET age = age + 5
+    WHERE job = 'student'
+''')
+
+
+def first_select():
+    print('First select =>\n')
+    cur.execute('''
+    SELECT name, sex, age, job FROM users
+    WHERE sex = "M" and
+    age BETWEEN 10 AND 30
+    ORDER BY age
+''')
+    for result in cur:
+        print(f'\t{result}')
+
+
+def second_select():
+    print('\nSecond select =>\n')
+    cur.execute('''
+    SELECT rowid, name FROM users
     WHERE name BETWEEN 'Александр Андреевич Живагин'
     AND 'Пелагея Ивановна Суворова'
     ORDER BY name
 ''')
+    for result in cur:
+        print(f'\t{result}')
 
-for result in cur:
-    print(result)
+
+
+
+with sq.connect('users.db') as con:
+    cur = con.cursor()
+    update_student()
+    delete_user_name()
+    set_req()
+    first_select()
+    second_select()
+    # drop_table()
+    # create_table()
+    # add()
+    # new_table()
